3bd6f85bbb2f64a934227a16cb3aed80d4f7d071
Ayush Yash Dwivedi <dwivediasd1020@gmail.com>
2026-07-12 23:25:13 +0530

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        String str = s.substring(s.lastIndexOf(' ')+1);
+        return str.length();
+        
+    }
+}
